ea9e8185b423e1553ef17036d4c67f342eb40fee
RidaAhmad <rida.ahmad@square63.com>
2016-06-29 11:43:52 +0500

    Movie: Added multiple posters functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -70,6 +70,6 @@ class MoviesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def movie_params
-      params.require(:movie).permit(:title, :description, :trailer, :featured, :approved)
+      params.require(:movie).permit(:title, :description, :trailer, attachments_attributes: [:id, :image, :_destroy])
     end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ActiveRecord::Base
   validates :title, presence: true, length: { maximum: 150 }
+
+  has_many :attachments, as: :attachable, dependent: :destroy
+
+  accepts_nested_attributes_for :attachments, allow_destroy: :true
 end

b654c3ae3b1ae130c1897513e45050f3b29089aa
RidaAhmad <rida.ahmad@square63.com>
2016-07-14 15:22:03 +0500

    Admin panel: Added Actor CRUD functionality

---
+++ app/admin/actor.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register Actor do
+
+  permit_params :list, :of, :attributes, :on, :model, :name, :bio, :gender
+
+  menu priority: 5
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :bio
+      f.input :gender, as: :select, collection: ['male', 'female'].map {|gender| [gender.titleize, gender]}, include_blank: false
+    end
+    f.actions
+  end
+
+end

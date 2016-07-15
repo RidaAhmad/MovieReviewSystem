1265a4be021ab32793e27fff153cf623e13c7756
RidaAhmad <rida.ahmad@square63.com>
2016-07-15 12:44:43 +0500

    Admin panel: Added report_count in Review

--- app/admin/review.rb
+++ app/admin/review.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Review do
 
-  permit_params :list, :of, :attributes, :on, :model, :comment, :movie_id, :user_id
+  permit_params :list, :of, :attributes, :on, :model, :comment, :report_count, :movie_id, :user_id
 
   menu priority: 4
 
@@ -9,15 +9,17 @@ ActiveAdmin.register Review do
       f.input :movie
       f.input :user_id, as: :select, collection: User.all.map{ |user| [user.email, user.id] }
       f.input :comment
+      f.input :report_count
     end
     f.actions
   end
 
-
   index do
+    id_column
     column :movie, sortable: :movie_id
     column :user, sortable: :user_id
     column :comment
+    column :report_count
     actions
   end
 
@@ -26,6 +28,7 @@ ActiveAdmin.register Review do
       row :movie
       row :user
       row :comment
+      row :report_count
       row :created_at
       row :updated_at
     end

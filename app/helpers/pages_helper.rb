8952cc517db051fea64da48f5a9bbb286f5b6279
RidaAhmad <rida.ahmad@square63.com>
2016-07-15 18:11:18 +0500

    User: Added email pattern validation

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -6,4 +6,8 @@ module PagesHelper
   def review_owner?(review_user, logged_in_user)
     review_user == logged_in_user if logged_in_user.present?
   end
+
+  def get_email_pattern
+    return '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,63}$'
+  end
 end

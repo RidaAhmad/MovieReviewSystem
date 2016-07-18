e0e6e38a9aecb750a0401b7960e7fc90dcb92625
RidaAhmad <rida.ahmad@square63.com>
2016-07-18 19:05:51 +0500

    User: Restricted current user from accessing other's profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
   before_action :authenticate_user!, only: [:show]
+  before_action :authenticate_current_user, only: [:show]
 
   def show
   end
@@ -9,5 +10,9 @@ class UsersController < ApplicationController
     def set_user
       @user = User.find(params[:id])
       @user_favorites = @user.favorites.page(params[:page])
+    end
+
+    def authenticate_current_user
+      redirect_to current_user, alert: 'Access Denied!' unless current_user == @user
     end
 end

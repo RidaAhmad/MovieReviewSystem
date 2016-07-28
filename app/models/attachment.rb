588ce22ec59b6acf73f229874aada6c13d627884
RidaAhmad <rida.ahmad@square63.com>
2016-07-28 13:26:07 +0500

    Attachment: Added validation for compulsory image attachment presence

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,7 @@
 class Attachment < ActiveRecord::Base
   has_attached_file :image, styles: { large: '450x400!', medium: '250x250!', thumb: '100x100!' }, default_url: ':style/missing.png'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_attachment_presence :image
 
   belongs_to :attachable, polymorphic: true
 end

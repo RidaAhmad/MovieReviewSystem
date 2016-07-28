82ca1692bd4eff89f18b7c9c2eca99bf4134a1a2
RidaAhmad <rida.ahmad@square63.com>
2016-07-28 15:30:52 +0500

    Rating: Added numericality validation to score

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,6 +3,7 @@ class Rating < ActiveRecord::Base
   belongs_to :user
 
   validates :movie_id, uniqueness: { scope: :user_id }
+  validates :score, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 
   scope :get_last_rating, -> (logged_in_user) { where(user_id: logged_in_user).last }
 end
